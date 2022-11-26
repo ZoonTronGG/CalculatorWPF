a5d058841a372cb024924a7b4198cd8b22549e11
Artyom Titorenko <vbymrf2@mail.ru>
2022-11-26 16:50:21 +0600

    Допилил весь калькулятор

--- CalculatorWPF/MainWindow.xaml.cs
+++ CalculatorWPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CalculatorWPF.MathMagic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,24 +22,34 @@ namespace CalculatorWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ResultProccesor _resultProccesor;
         public MainWindow()
         {
             InitializeComponent();
+            InitializeResultProccesor();
         }
-        
+
+        private void InitializeResultProccesor()
+        {
+            _resultProccesor = new ResultProccesor();
+        }
         private void ButtonNumber_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Ввод числа");
+            Button btn = sender as Button;
+            string content = btn.Content.ToString();
+            _resultProccesor.AddNumberToTextBlock(content);
         }
 
         private void ButtonOperation_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Операция");
+            Button btn = sender as Button;
+            _resultProccesor.PrepareForOperation(btn.Content.ToString());
         }
 
         private void buttonsWindow_Loaded(object sender, RoutedEventArgs e)
         {
             string numbers = "0 1 2 3 4 5 6 7 8 9";
+
             foreach (Button b in buttonsWindow.Children)
             {
                 if (b.Content != null)
---
+++ CalculatorWPF/MathMagic/ResultProccesor.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CalculatorWPF.MathMagic
+{
+    public enum Operation
+    {
+        Substract = 1,
+        Sum,
+        Mult,
+        Divide,
+        Empty
+    }
+    public class ResultProccesor
+    {
+        public MainWindow MainWindow { get; set; }
+        public string FirstNumber { get; set; }
+        public Operation Operation { get; set; } = Operation.Empty;
+        public string OperationMathSign { get; set; }
+        public string SecondNumber { get; set; }
+        public string Result { get; set; }
+        public ResultProccesor()
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.GetType() == typeof(MainWindow))
+                {
+                    MainWindow = (window as MainWindow);
+                }
+            }
+        }
+        public void AddNumberToTextBlock(string number)
+        {
+            MainWindow.txtResult.FontSize = 60;
+            if (MainWindow.txtResult.Text == "0")
+            {
+                MainWindow.txtResult.Text = number;
+            }
+            else
+            {
+                //Ограничение в 12 символов
+                if (MainWindow.txtResult.Text.Length < 12)
+                {
+                    MainWindow.txtResult.Text += number;
+                }
+            }
+
+            if (FirstNumber == null || Operation == Operation.Empty)
+            {
+                FirstNumber = MainWindow.txtResult.Text;
+            }
+            else
+            {
+                SecondNumber = MainWindow.txtResult.Text;
+            }
+
+        }
+
+        public void PrepareForOperation(string operation)
+        {
+            //Очищаем поле для второго числа
+            if (FirstNumber == null || FirstNumber == "Error")
+            {
+                FirstNumber = "0";
+                Operation = Operation.Empty;
+                return;
+            }
+            if (operation == "CE")
+            {
+                if (FirstNumber != null)
+                {
+                    SecondNumber = "0";
+                }
+                else
+                {
+                    FirstNumber = "0";
+                }
+                Operation = Operation.Empty;
+                MainWindow.txtResult.Text = "0";
+                MainWindow.history.Text = "";
+                return;
+            }
+            if (operation == "C")
+            {
+                BackToNormalState();
+                return;
+            }
+            if (operation == ".")
+            {
+                if (!MainWindow.txtResult.Text.Contains('.'))
+                {
+                    MainWindow.txtResult.Text += ".";
+                }
+                return;
+            }
+
+            if (operation == "<")
+            {
+                bool isFirstNChosen = MainWindow.txtResult.Text == FirstNumber ? 
+                    true: false;
+
+                MainWindow.txtResult.Text = MainWindow.txtResult.Text
+                    .Remove(MainWindow.txtResult.Text.Length - 1);
+
+                if (isFirstNChosen)
+                {
+                    FirstNumber = MainWindow.txtResult.Text;
+                }
+                else
+                {
+                    SecondNumber = MainWindow.txtResult.Text;
+                }
+
+                if (MainWindow.txtResult.Text.Length == 0)
+                {
+                    MainWindow.txtResult.Text = "0";
+                }
+                return;
+            }
+
+
+            MainWindow.txtResult.Text = "0";
+            switch (operation)
+            {
+                case "-":
+                    Operation = Operation.Substract;
+                    OperationMathSign = "-";
+                    break;
+                case "+":
+                    Operation = Operation.Sum;
+                    OperationMathSign = "+";
+                    break;
+                case "*":
+                    Operation = Operation.Mult;
+                    OperationMathSign = "*";
+                    break;
+                case "/":
+                    Operation = Operation.Divide;
+                    OperationMathSign = "/";
+                    break;
+                case "=":
+                    Result = FindResult();
+                    MainWindow.txtResult.Text = Result;
+                    // Тут присвоить историю 
+                    MainWindow.history.Text += $"{FirstNumber} {OperationMathSign} " +
+                        $"{SecondNumber} = {Result}|";
+                    // Critically important
+                    FirstNumber = Result;
+                    if (FirstNumber == "Error")
+                    {
+                        BackToNormalState();
+                    }
+                    break;
+                default:
+                    Operation = Operation.Empty;
+                    break;
+            }
+
+        }
+        private void BackToNormalState()
+        {
+            FirstNumber = null;
+            SecondNumber = null;
+            Result = null;
+            MainWindow.txtResult.Text = "0";
+            Operation = Operation.Empty;
+            MainWindow.txtResult.FontSize = 60;
+        }
+
+        private string FindResult()
+        {
+            bool isNumeric1 = int.TryParse(FirstNumber, out int n1);
+            bool isNumeric2 = int.TryParse(SecondNumber, out int n2);
+
+            if (isNumeric1 && isNumeric2)
+            {
+                return PerformOperationInt(n1, n2);
+            }
+
+            bool isDouble1 = double.TryParse(FirstNumber, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out double d1);
+            bool isDouble2 = double.TryParse(SecondNumber, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out double d2);
+           
+            if (isDouble1 && isDouble2)
+            {
+                return PerformOperationDouble(d1, d2);
+            }
+            MainWindow.txtResult.FontSize = 15;
+            return "Error";
+        }
+
+        private string PerformOperationInt(int num1, int num2)
+        {
+            int res = 0;
+            if (Operation == Operation.Divide)
+            {
+                if (num2 != 0)
+                {
+                    res = (num1 / num2);
+                }
+            }
+            if (Operation == Operation.Mult)
+            {
+                res = (num1 * num2);
+            }
+            if (Operation == Operation.Substract)
+            {
+                res = (num1 - num2);
+            }
+            if (Operation == Operation.Sum)
+            {
+                res = (num1 + num2);
+            }
+            if (res == 0)
+            {
+                MainWindow.txtResult.FontSize = 15;
+                return "Error";
+            }
+
+            return res.ToString();
+        }
+
+        // poka ne pridumal kak eto izmenit
+        private string PerformOperationDouble(double num1, double num2)
+        {
+            double res = 0.0;
+            if (Operation == Operation.Divide)
+            {
+                res = (num1 / num2);          
+            }
+            if (Operation == Operation.Mult)
+            {
+                res = (num1 * num2);
+            }
+            if (Operation == Operation.Substract)
+            {
+                res = (num1 - num2);
+            }
+            if (Operation == Operation.Sum)
+            {
+                res = (num1 + num2);
+            }
+            if (res == 0.0)
+            {
+                MainWindow.txtResult.FontSize = 15;
+                return "Error";
+            }
+
+            return res.ToString().Replace(',', '.');
+        }
+
+
+    }
+}
